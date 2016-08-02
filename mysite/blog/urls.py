6e00d1001cba8f3cf18df820da04e1c9d11558f6
Ricardo Ferraz Leal <ricleal@gmail.com>
2016-08-02 18:37:06 -0400

    Added create form

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,8 +1,9 @@
 #coding: utf-8
 from django.conf.urls import patterns, url
-from blog.views import PostListView, PostDetailview
+from blog.views import PostListView, PostDetailView,PostCreateView
 
 urlpatterns = [
     url(r'^$', PostListView.as_view(), name='list'),
-    url(r'^(?P<pk>\d+)/$', PostDetailview.as_view(),name='detail')
+    url(r'^(?P<pk>\d+)/$', PostDetailView.as_view(),name='detail'),
+    url(r'^create/$', PostCreateView.as_view(),name='create'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,11 +1,16 @@
 #from django.shortcuts import render
 from blog.models import Post
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 
 
 class PostListView(ListView):
     model = Post
 
 
-class PostDetailview(DetailView):
+class PostDetailView(DetailView):
     model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = '__all__'
